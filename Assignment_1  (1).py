a0a22de3e38843435bc3dd1d3b5f98daa5f37c68
ApicaSharma15 <85416479+ApicaSharma15@users.noreply.github.com>
2022-01-31 03:57:05 +0530

    Add files via upload
    Assignment1

---
+++ Assignment_1  (1).py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#1 Expression or values?
+# * = expression
+# hello = value
+# -87.8 = value
+# - = expression
+# / = expression
+# + = expression 
+
+
+# In[2]:
+
+
+#2 String vs Variable ?
+# Variables store information in a program.You can think of variables like a box or a container with a name on them, related to the things inside. 
+# A string is a type of value that can be stored in a variable. A string is made up of characters, and can include letters, words, phrases, or symbols.
+
+
+# In[3]:
+
+
+#3 Types of data ? 
+# Text Type:	str
+#Numeric Types:	int, float, complex
+#Sequence Types:	list, tuple, range
+#Mapping Type:	dict
+#Set Types:	set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+
+
+# In[4]:
+
+
+#4  What's expression made of? What expressions do?
+# An expression is a combination of operators and operands that is interpreted to produce some other value. In any programming language, an expression is evaluated as per the precedence of its operators. So that if there is more than one operator in an expression, their precedence decides which operation will be performed first. We have many different types of expressions in Python.
+# Constant Expressions: These are the expressions that have constant values only.
+# Arithmetic Expressions: An arithmetic expression is a combination of numeric values, operators, and sometimes parenthesis. The result of this type of expression is also a numeric value. The operators used in these expressions are arithmetic operators like addition, subtraction, etc.
+# Integral Expressions: These are the kind of expressions that produce only integer results after all computations and type conversions.
+# Floating Expressions: These are the kind of expressions which produce floating point numbers as result after all computations and type conversions.
+# Relational Expressions: In these types of expressions, arithmetic expressions are written on both sides of relational operator (> , < , >= , <=). Those arithmetic expressions are evaluated first, and then compared as per relational operator and produce a boolean output in the end. These expressions are also called Boolean expressions.
+# Logical Expressions: These are kinds of expressions that result in either True or False. It basically specifies one or more conditions. For example, (10 == 9) is a condition if 10 is equal to 9. As we know it is not correct, so it will return False. Studying logical expressions, we also come across some logical operators which can be seen in logical expressions most often.
+# Bitwise Expressions: These are the kind of expressions in which computations are performed at bit level.
+# Combinational Expressions: We can also use different types of expressions in a single expression, and that will be termed as combinational expressions.
+
+
+# In[5]:
+
+
+#5 difference between expression and statement?
+# In programming language terminology, an “expression” is a combination of values and functions that are combined and interpreted by the compiler to create a new value, as opposed to a “statement” which is just a standalone unit of execution and doesn't return anything.
+
+
+# In[6]:
+
+
+#6 
+bacon = 22
+bacon+ 1 
+
+
+# In[8]:
+
+
+#6 
+#code produces 23 
+
+
+# In[9]:
+
+
+#7
+'spam' + 'spamspam'
+'spam'* 3 
+
+
+# In[10]:
+
+
+#7
+# 'spamspamspam'
+
+
+# In[11]:
+
+
+#8 Why is eggs a valid variable name while 100 is invalid?
+#Because variable names cannot begin with a number.
+
+
+# In[12]:
+
+
+#9 What three functions can be used to get the integer, floating-point number, or string version of a value?
+# The int() , float() , and str( ) functions will evaluate to the integer, floating-point number, and string versions of the value passed to them.
+
+
+# In[13]:
+
+
+#10 ' I have eaten' + 99 + 'burritos' ? 
+' I have eaten' + 99 + 'burritos'
+
+
+# In[15]:
+
+
+' I have eaten' + '99' + 'burritos'
+
+
+# In[ ]:
+
+
+# 10
+make 99 a string value 
+
