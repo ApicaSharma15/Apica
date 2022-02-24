568fa9835fc5e6c57867db259b3ec408efd722f3
ApicaSharma15 <85416479+ApicaSharma15@users.noreply.github.com>
2022-02-25 03:14:06 +0530

    Add files via upload

---
+++ Assignment_20.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#1. Set the variable test1 to the string 'This is a test of the emergency text system,' and save test1 to a file named test.txt.
+test1 = 'This is a test of the emergency text system,'
+file = open('test.txt','w')
+file.write(test1)
+
+
+# In[6]:
+
+
+#2. Read the contents of the file test.txt into the variable test2. Is there a difference between test 1 and test 2?
+test1 = 'This is a test of the emergency text system,'
+file = open('test.txt','w')
+file2 = open('test.txt','r')
+test2 = file2.readline()
+
+test2
+
+
+# In[7]:
+
+
+if test1==test2:
+    print('Both are same')
+
+
+# In[ ]:
+
+
+
+#3. Create a CSV file called books.csv by using these lines:
+## title,author,year
+## The Weirdstone of Brisingamen,Alan Garner,1960
+## Perdido Street Station,China Miéville,2000
+## Thud!,Terry Pratchett,2005
+## The Spellman Files,Lisa Lutz,2007
+## Small Gods,Terry Pratchett,1992
+
+
+# In[9]:
+
+
+import csv
+rows =[ ['title','author','year'],
+    ['The Weirdstone of Brisingamen','Alan Garner',1960],
+    ['Perdido Street Station','China Miéville',2000],
+    ['Thud!','Terry Pratchett',2005],
+    ['The Spellman Files','Lisa Lutz',2007],
+    ['Small Gods','Terry Pratchett',1992]]
+with open('books.csv','w',newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(rows)
+
+
+# In[ ]:
+
+
+
+
+
+# In[31]:
+
+
+#4. Use the sqlite3 module to create a SQLite database called books.db, and a table called books with these fields: title (text), author (text), and year (integer).
+import sqlite3
+db= sqlite3.connect('books.db')
+curs= db.cursor()
+curs.execute('''create table book(title text, author text,  year int)''')
+db.commit
+
+
+# In[33]:
+
+
+#5. Read books.csv and insert its data into the book table.
+import csv
+import sqlite3
+ins_str = 'insert into book values(?,?,?)'
+with open ('books.csv','rt') as infile:
+    books= csv.DictReader(infile)
+    for book in books:
+        curs.execute(ins_str,(book['title'], book['author'], book['year']))
+db.commit 
+
+
+# In[18]:
+
+
+#6. Select and print the title column from the book table in alphabetical order.
+c.execute('select title from books order by title asc')
+print(c.fetchall())
+
+
+# In[19]:
+
+
+#7. From the book table, select and print all columns in the order of publication.
+c.execute('select title, author,year from books order by year')
+
+
+df = pd.DataFrame(c.fetchall(), columns=['title','author','year'])
+df
+
+
+# In[20]:
+
+
+#8. Use the sqlalchemy module to connect to the sqlite3 database books.db that you just made in exercise 6.
+import sqlalchemy
+engine = sqlalchemy.create_engine("sqlite:///books.db")
+rows = engine.execute('select * from books')
+for i in rows:
+    print(i)
+
+
+# In[25]:
+
+
+#9. . Install the Redis server and the Python redis library (pip install redis) on your computer. Create a Redis hash called test with the fields count (1) and name ('Fester Bestertester'). Print all the fields for test.
+get_ipython().system('pip install redis')
+
+
+# In[ ]:
+
+
+import redis
+conn = redis.Redis()
+conn.delete('test')
+>>
+conn.hmset('test', {'count': 1, 'name': 'Fester Bestertester'})
+>>
+conn.hgetall('test')
+
+
+# In[ ]:
+
+
+#10. Increment the count field of test and print it.
+conn.hincrby('test','count', 3)
+conn.hget('test', 'count')
+
